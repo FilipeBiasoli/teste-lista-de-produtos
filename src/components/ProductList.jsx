a89c5a02d17fb86fb17e356330096b907b0fb052
Filipe Biasoli <filipe.biasoli@gmail.com>
2023-03-30 13:51:45 -0300

    mais produtos na lista

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,32 +3,156 @@ import ItemList from './ItemList';
 // images
 import Monitor01 from '../assets/img/monitor/monitor01.jpg';
 import Monitor02 from '../assets/img/monitor/monitor02.jpg';
+import Keyboard01 from '../assets/img/keyboard/keyboard01.jpg';
+import Keyboard02 from '../assets/img/keyboard/keyboard02.jpg';
+import Notebook01 from '../assets/img/notebook/notebook01.jpg';
+import Notebook02 from '../assets/img/notebook/notebook02.jpg';
 
 function ProductList() {
   return (
     <div id="ProductList" className="container">
       <ItemList
-        image={[Monitor01, Monitor02]}
-        title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
-        price={['R$ 2.813,99', 'R$ 2.599,00']}
-        installments="12x de R$ 260,58"
         id="1"
-      />
-
-      <ItemList
         image={[Monitor01, Monitor02]}
         title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
         price={['R$ 2.813,99', 'R$ 2.599,00']}
         installments="12x de R$ 260,58"
+      />
+
+      <ItemList
         id="2"
+        image={[Keyboard02, Keyboard01]}
+        title="Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, Layout US"
+        price={['R$ 352,93', 'R$ 249,99']}
+        installments="10x de R$ 29,41"
       />
 
       <ItemList
+        id="3"
+        image={[Notebook01, Notebook02]}
+        title="ACER Notebook Gamer Nitro 5 AN515-57-585H, CI5 11400H, 8GB, 1TB SDD, (NVIDIA GTX 1650)"
+        price={['R$ 5.899,00', 'R$ 5.450,00']}
+        installments="10x de R$ 545,00"
+      />
+
+      <ItemList
+        id="4"
         image={[Monitor01, Monitor02]}
         title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
         price={['R$ 2.813,99', 'R$ 2.599,00']}
         installments="12x de R$ 260,58"
-        id="3"
+      />
+
+      <ItemList
+        id="5"
+        image={[Keyboard02, Keyboard01]}
+        title="Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, Layout US"
+        price={['R$ 352,93', 'R$ 249,99']}
+        installments="10x de R$ 29,41"
+      />
+
+      <ItemList
+        id="6"
+        image={[Notebook01, Notebook02]}
+        title="ACER Notebook Gamer Nitro 5 AN515-57-585H, CI5 11400H, 8GB, 1TB SDD, (NVIDIA GTX 1650)"
+        price={['R$ 5.899,00', 'R$ 5.450,00']}
+        installments="10x de R$ 545,00"
+      />
+
+      <ItemList
+        id="7"
+        image={[Monitor01, Monitor02]}
+        title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
+        price={['R$ 2.813,99', 'R$ 2.599,00']}
+        installments="12x de R$ 260,58"
+      />
+
+      <ItemList
+        id="8"
+        image={[Keyboard02, Keyboard01]}
+        title="Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, Layout US"
+        price={['R$ 352,93', 'R$ 249,99']}
+        installments="10x de R$ 29,41"
+      />
+
+      <ItemList
+        id="9"
+        image={[Notebook01, Notebook02]}
+        title="ACER Notebook Gamer Nitro 5 AN515-57-585H, CI5 11400H, 8GB, 1TB SDD, (NVIDIA GTX 1650)"
+        price={['R$ 5.899,00', 'R$ 5.450,00']}
+        installments="10x de R$ 545,00"
+      />
+
+      <ItemList
+        id="10"
+        image={[Monitor01, Monitor02]}
+        title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
+        price={['R$ 2.813,99', 'R$ 2.599,00']}
+        installments="12x de R$ 260,58"
+      />
+
+      <ItemList
+        id="11"
+        image={[Keyboard02, Keyboard01]}
+        title="Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, Layout US"
+        price={['R$ 352,93', 'R$ 249,99']}
+        installments="10x de R$ 29,41"
+      />
+
+      <ItemList
+        id="12"
+        image={[Notebook01, Notebook02]}
+        title="ACER Notebook Gamer Nitro 5 AN515-57-585H, CI5 11400H, 8GB, 1TB SDD, (NVIDIA GTX 1650)"
+        price={['R$ 5.899,00', 'R$ 5.450,00']}
+        installments="10x de R$ 545,00"
+      />
+
+      <ItemList
+        id="13"
+        image={[Monitor01, Monitor02]}
+        title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
+        price={['R$ 2.813,99', 'R$ 2.599,00']}
+        installments="12x de R$ 260,58"
+      />
+
+      <ItemList
+        id="14"
+        image={[Keyboard02, Keyboard01]}
+        title="Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, Layout US"
+        price={['R$ 352,93', 'R$ 249,99']}
+        installments="10x de R$ 29,41"
+      />
+
+      <ItemList
+        id="15"
+        image={[Notebook01, Notebook02]}
+        title="ACER Notebook Gamer Nitro 5 AN515-57-585H, CI5 11400H, 8GB, 1TB SDD, (NVIDIA GTX 1650)"
+        price={['R$ 5.899,00', 'R$ 5.450,00']}
+        installments="10x de R$ 545,00"
+      />
+
+      <ItemList
+        id="16"
+        image={[Monitor01, Monitor02]}
+        title='Monitor LED 27" Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50'
+        price={['R$ 2.813,99', 'R$ 2.599,00']}
+        installments="12x de R$ 260,58"
+      />
+
+      <ItemList
+        id="17"
+        image={[Keyboard02, Keyboard01]}
+        title="Teclado Mecânico Gamer HyperX Alloy MKW100, RGB, Switch Red, Full Size, Layout US"
+        price={['R$ 352,93', 'R$ 249,99']}
+        installments="10x de R$ 29,41"
+      />
+
+      <ItemList
+        id="18"
+        image={[Notebook01, Notebook02]}
+        title="ACER Notebook Gamer Nitro 5 AN515-57-585H, CI5 11400H, 8GB, 1TB SDD, (NVIDIA GTX 1650)"
+        price={['R$ 5.899,00', 'R$ 5.450,00']}
+        installments="10x de R$ 545,00"
       />
     </div>
   );
